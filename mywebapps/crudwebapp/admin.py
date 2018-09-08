4493afb40a9dc2d190cbfd660baaf863df4ca304
omkar-hardwell <omkarkshirsagar862@gmail.com>
2018-09-08 20:55:13 +0530

    Task-4: Created superuser and registered Employee and Department models

--- mywebapps/crudwebapp/admin.py
+++ mywebapps/crudwebapp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Department
+from .models import Employee
+
 
 # Register your models here.
+admin.site.register(Department)
+admin.site.register(Employee)

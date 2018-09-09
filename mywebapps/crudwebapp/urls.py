4c57c84a1277d93b588a679f0a31535b558fd656
omkar-hardwell <omkarkshirsagar862@gmail.com>
2018-09-09 20:54:01 +0530

    Task-5: Created view for department model

--- mywebapps/crudwebapp/urls.py
+++ mywebapps/crudwebapp/urls.py
@@ -1,1 +1,9 @@
 """crudwebapp URL Configuration."""
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('departments/', views.departments, name='department')
+]
--- mywebapps/crudwebapp/views.py
+++ mywebapps/crudwebapp/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Department
+
 
 # Create your views here.
+def index(request):
+    """Index view.
+    :param request: Obj - Request.
+    :return: str - HttpResponse response.
+    """
+    return HttpResponse("Hello, world. You're at the index.")
+
+
+def departments(request):
+    """Show departments detail.
+    :param request: Obj - Request.
+    :return: Render to department view.
+    """
+    department_list = Department.objects.all()
+    return render(
+        request, 'departments.html', {'departments': department_list})
